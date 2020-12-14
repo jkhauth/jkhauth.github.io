0e9d59b1ac6168ce9756bcce546fa6bfa648b0d8
jkhauth <jkhauth@gmail.com>
2020-12-14 07:47:36 -0500

    seeing if it looks different on mobile

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,22 @@ target.onclick = ({ target }) =>  neonGlory(target);
 $('.carousel').carousel({
     interval: false
   })
+//FIRST H2
+const target2 = window.document.getElementsByTagName('h1')[1]
+
+
+const flickerLetter2 = letter => `<span style="animation: text-flicker-in-glow ${Math.random()*4}s linear both ">${letter}</span>`
+const colorLetter2 = letter => `<span style="color: hsla(${Math.random()*360}, 100%, 80%, 1);">${letter}</span>`;
+const flickerAndColorText2 = text => 
+  text
+    .split('')
+    .map(flickerLetter2)
+    .map(colorLetter2)
+    .join('');
+const neonGlory2 = target => target.innerHTML = flickerAndColorText2(target.textContent);
+neonGlory2(target2);
+target2.onclick = ({ target }) =>  neonGlory2(target);
+
+$('.carousel').carousel({
+    interval: false
+  })
