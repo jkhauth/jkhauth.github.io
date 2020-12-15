9125f57bb1fb8e140c8a20b195832f826acdfb75
John Hauth <jkhauth@gmail.com>
2020-12-15 16:21:55 -0500

    added portfolio styles, resume link

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,16 +1,30 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+$("#portfoliocontent").hide()
+
 $(document).ready(function(){
     $("#portfolio-btn").click(function(){
-        $('#homepage').slideToggle(600);
-        $("#portfolio").delay(600).slideToggle(600);
+        $('#aboutmesection').slideToggle(600);
+        $("#portfoliocontent").delay(600).slideToggle(600);
 });
+})
 
-portfoliobtn();
-function portfoliobtn(){
-    var x = window.matchMedia("(max-width: 700px)" || "(max-width:300px")
-    if (x.matches) {
-        $("#portfolio-btn").click(function(){
-            window.location.href = "portfolio.html"
-        });
-        }
-    }
+$(document).ready(function(){
+  $("#home-btn").click(function(){
+      $('#portfoliocontent').slideToggle(600);
+      $("#aboutmesection").delay(600).slideToggle(600);
 });
+})
