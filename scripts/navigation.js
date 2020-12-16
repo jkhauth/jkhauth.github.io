9ef8edc87779483a840f461bdb75b9ecf7a49cff
John Hauth <jkhauth@gmail.com>
2020-12-15 22:27:36 -0500

    photo functionality for certain screens

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -28,3 +28,15 @@ $(document).ready(function(){
       $("#aboutmesection").delay(600).slideToggle(600);
 });
 })
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    $('#main-photo').hide()
+  } else {
+    $('#main-photo').show()
+  }
+}
+
+var x = window.matchMedia("(max-width: 800px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
