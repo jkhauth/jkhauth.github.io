bcad4a0821920b9043e2358be206c2545138758b
John Hauth <jkhauth@gmail.com>
2021-01-06 11:58:40 -0500

    fix background gradient

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,3 @@
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-   
-    document.body.style.backgroundImage = "none"
-    document.body.style.backgroundColor = "#2b0fde"
-  } else {
-  }
-}
-
-var x = window.matchMedia("(max-width: 800px)" || "(max-width: 300px)")
-myFunction(x)
 
 
 
