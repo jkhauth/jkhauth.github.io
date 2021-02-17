037d1c69d47953458cb030028909ab893aab5957
John Hauth <jkhauth@gmail.com>
2021-02-16 19:42:41 -0500

    update fix

--- src/components/home.js
+++ src/components/home.js
@@ -12,7 +12,7 @@ return (
      <h1 className="text-center">A MERN Stack Developer</h1>
      <h1 className="text-center">Ready to build together?</h1>
      <div className="intro-text">
-     <p className="text-center text-justify">Coming from an experienced business background, I take pride in the ablity to communicate with clients to go above and beyond their expectations. I am currently looking for employment in development and have a passion for front-end UX-UI design.</p>
+     <p className="text-center text-justify">Coming from an experienced business background, I take pride in the ablity to communicate with clients to go above and beyond their expectations. I am currently looking for employment in development and trying new/creating hot sauces!</p>
      </div>
  </Fade>
  </div>
